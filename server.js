966253a387c5892c428652b85ce493ea0be3ec62
Somsubhra Das <das.somsubhra1@gmail.com>
2019-06-20 00:08:33 +0530

    production environment setup

--- server.js
+++ server.js
@@ -6,19 +6,45 @@ const app = express();
 // File upload middleware
 app.use(fileUpload());
 
+// Serve static assets if it's production environment
+if (process.env.NODE_ENV === "production") {
+    // Set static folder
+    app.use(express.static("client/build"));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    });
+}
+
 // Upload endpoint
 app.post("/upload", (req, res) => {
     if (req.files === null) {
         return res.status(400).json({ msg: "No file was uploaded" });
     }
     const file = req.files.file;
-    file.mv(`${__dirname}/client/public/uploads/${file.name}`, err => {
-        if (err) {
-            console.error(err);
-            return res.status(500).json(err);
-        }
-        res.json({ fileName: file.name, filePath: `/uploads/${file.name}` });
-    });
+
+    if (process.env.NODE_ENV === "production") {
+        file.mv(`${__dirname}/client/build/uploads/${file.name}`, err => {
+            if (err) {
+                console.error(err);
+                return res.status(500).json(err);
+            }
+            res.json({
+                fileName: file.name,
+                filePath: `uploads/${file.name}`
+            });
+        });
+    } else {
+        file.mv(`${__dirname}/client/public/uploads/${file.name}`, err => {
+            if (err) {
+                console.error(err);
+                return res.status(500).json(err);
+            }
+            res.json({
+                fileName: file.name,
+                filePath: `uploads/${file.name}`
+            });
+        });
+    }
 });
 
 const PORT = process.env.PORT || 5000;
