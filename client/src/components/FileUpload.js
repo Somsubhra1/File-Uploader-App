f4ba5a837eaa5223ccf41738bcfeececf9aa8d29
Somsubhra Das <das.somsubhra1@gmail.com>
2019-06-19 23:05:29 +0530

    code for displaying message done

--- client/src/components/FileUpload.js
+++ client/src/components/FileUpload.js
@@ -1,10 +1,12 @@
 import React, { Fragment, useState } from "react";
 import axios from "axios";
+import Message from "./Message";
 
 const FileUpload = () => {
     const [file, setFile] = useState("");
     const [filename, setFilename] = useState("Choose File");
     const [uploadedFile, setuploadedFile] = useState({});
+    const [message, setMessage] = useState("");
 
     const onChange = e => {
         setFile(e.target.files[0]); // first one file from file
@@ -25,56 +27,51 @@ const FileUpload = () => {
             .then(res => {
                 const { fileName, filePath } = res.data;
                 setuploadedFile({ fileName, filePath });
+                setMessage("File uploaded");
             })
             .catch(err => {
                 if (err.response.status === 500) {
-                    console.log("There was a problem with the server");
+                    setMessage("There was a problem with the server");
                 } else {
-                    console.log(err.response.data.msg);
+                    setMessage(err.response.data.msg);
                 }
             });
     };
 
     return (
-        <div>
-            <Fragment>
-                <form action="" onSubmit={onSubmit}>
-                    <div className="custom-file mb-4">
-                        <input
-                            type="file"
-                            className="custom-file-input"
-                            id="customFile"
-                            onChange={onChange}
-                        />
-                        <label
-                            className="custom-file-label"
-                            htmlFor="customFile"
-                        >
-                            {filename}
-                        </label>
-                    </div>
+        <Fragment>
+            {message ? <Message msg={message} /> : null}
+            <form action="" onSubmit={onSubmit}>
+                <div className="custom-file mb-4">
                     <input
-                        type="submit"
-                        value="Upload"
-                        className="btn btn-primary btn-block mt-4"
+                        type="file"
+                        className="custom-file-input"
+                        id="customFile"
+                        onChange={onChange}
                     />
-                </form>
-                {uploadedFile ? (
-                    <div className="row mt-5">
-                        <div className="col-md-6 m-auto">
-                            <h3 className="text-center">
-                                {uploadedFile.fileName}
-                            </h3>
-                            <img
-                                src={uploadedFile.filePath}
-                                style={{ width: "100%" }}
-                                alt={uploadedFile.fileName}
-                            />
-                        </div>
+                    <label className="custom-file-label" htmlFor="customFile">
+                        {filename}
+                    </label>
+                </div>
+                <input
+                    type="submit"
+                    value="Upload"
+                    className="btn btn-primary btn-block mt-4"
+                />
+            </form>
+            {uploadedFile ? (
+                <div className="row mt-5">
+                    <div className="col-md-6 m-auto">
+                        <h3 className="text-center">{uploadedFile.fileName}</h3>
+                        <img
+                            src={uploadedFile.filePath}
+                            style={{ width: "100%" }}
+                            alt={uploadedFile.fileName}
+                        />
                     </div>
-                ) : null}
-            </Fragment>
-        </div>
+                </div>
+            ) : null}
+        </Fragment>
     );
 };
 
